c209ce1c9589b0cb1733e2202110921c504c5091
sshah711 <sshah380008@gmail.com>
2025-07-31 14:19:46 +0530

    changes done

--- backend/models/department.js
+++ backend/models/department.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const departmentSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true }
+  id: { type: String, unique: true },
+  name: { type: String, required: true },
 });
 
 module.exports = mongoose.model('Department', departmentSchema);
